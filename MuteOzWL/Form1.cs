293317b88e6473cc5c92ac36a2349c741f61e6b3
Gihun Ham <progh2@gmail.com>
2020-03-27 09:18:20 +0900

    CurrentUser레지스트리를 참조하는 케이스도 있어서 반영

--- MuteOzWL/Form1.cs
+++ MuteOzWL/Form1.cs
@@ -18,6 +18,7 @@ namespace RegistryKeyTest
         const string KEY_VALUE = @"~ RUNASADMIN";
         string KEY;
         RegistryKey reg;
+        RegistryKey reg2;
 
         public Form1()
         {
@@ -40,9 +41,11 @@ namespace RegistryKeyTest
         private void getStatus()
         {
             reg = Registry.LocalMachine.CreateSubKey(REG_ADDRESS);
+            reg2 = Registry.CurrentUser.CreateSubKey(REG_ADDRESS);
             string key_value = Convert.ToString(reg.GetValue(KEY, ""));
-            
-            if (reg.ValueCount > 0 && key_value == KEY_VALUE)
+            string key_value2 = Convert.ToString(reg2.GetValue(KEY, ""));
+
+            if ((reg.ValueCount > 0 && key_value == KEY_VALUE) && (reg2.ValueCount > 0 && key_value2 == KEY_VALUE))
             {
                 toolStripStatusLabel2.Text = "알림 끄기가 적용되어 있습니다";
                 btnMuteOn.Enabled = false;
@@ -56,13 +59,17 @@ namespace RegistryKeyTest
             }
 
             reg.Close();
+            reg2.Close();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             reg = Registry.LocalMachine.CreateSubKey(REG_ADDRESS);
+            reg2 = Registry.CurrentUser.CreateSubKey(REG_ADDRESS);
             reg.SetValue(KEY, KEY_VALUE);
+            reg2.SetValue(KEY, KEY_VALUE);
             reg.Close();
+            reg2.Close();
             MessageBox.Show("알림 끄기가 적용되었습니다.\n완전히 반영이 되려면 컴퓨터를  재부팅하셔야 합니다.");
             getStatus();
         }
@@ -70,11 +77,17 @@ namespace RegistryKeyTest
         private void button2_Click(object sender, EventArgs e)
         {
             reg = Registry.LocalMachine.CreateSubKey(REG_ADDRESS);
+            reg2 = Registry.CurrentUser.CreateSubKey(REG_ADDRESS);
             if (reg.GetValue(KEY) != null)
             {
                 reg.DeleteValue(KEY);
             }
+            if (reg2.GetValue(KEY) != null)
+            {
+                reg2.DeleteValue(KEY);
+            }
             reg.Close();
+            reg2.Close();
             MessageBox.Show("알림 끄기가 취소되었습니다.\n완전히 반영이 되려면 컴퓨터를 재부팅하셔야 합니다.");
             getStatus();
         }
